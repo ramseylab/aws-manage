58de4a1904727217af05db85198b8111801a47a7
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2018-03-10 10:07:57 -0800

    updated for pending status

--- manage-instances-cgi.py
+++ manage-instances-cgi.py
@@ -3,17 +3,6 @@ import boto3
 import cgi
 import cgitb
 cgitb.enable()
-#import argparse
-#import json
-
-# parser = argparse.ArgumentParser(description="Lists, stops, and starts EC2 instances for the Ramsey Lab",
-#                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-# parser.add_argument('command', metavar='command', type=str, help='the command to be run', choices=["list","stop","start"])
-# parser.add_argument('--instance-id', action='store', metavar='instance_id', type=str, help='the AWS instance ID of the instance to stop or start', default=None)
-
-# args = parser.parse_args()
-# command = args.command
-# req_instance_id = args.instance_id
 
 result = dict()
 ec2 = boto3.client("ec2")
@@ -91,11 +80,12 @@ for reservation in response['Reservations']:
         if keep_instance:
             instance_id = instance["InstanceId"]
             manager_passcodes[instance_id] = manager_passcode
-            print("<tr><td><input type=\"radio\" name=\"target_instance_id\" value=\"" + instance_id + "\" />" + instance_name + "</td><td>" + instance["State"]["Name"] + "</td><td>" + instance_id + "</td></tr>")
+            instance_state = instance["State"]["Name"]
+            if instance_state != "pending":
+                print("<tr><td><input type=\"radio\" name=\"target_instance_id\" value=\"" + instance_id + "\" />" + instance_name + "</td><td>" + instance_state + "</td><td>" + instance_id + "</td></tr>")
+            else:
+                print("<tr><td>" + instance_name + "</td><td>" + instance_state + "</td><td>" + instance_id + "</td></tr>")
             
-#            result['list_data'].append({'instance-id': instance['InstanceId'],
-#                                        'instance-state': instance['State']['Name'],
-#                                        'instance-name': instance_name})
 print("</table>")
 
 print("<p><input type=\"radio\" name=\"command\" value=\"start\" />Start instance</p>")
@@ -108,45 +98,4 @@ print("</form>")
 print("</body>")
 print("</html>")
             
-# if command == "list":
-#     response = ec2.describe_instances()
-#     result['list_data'] = []
-#     for reservation in response['Reservations']:
-#         for instance in reservation['Instances']:
-#             instance_tags = instance['Tags']
-#             keep_instance = False
-#             for tag in instance_tags:
-#                 if tag['Key'] == 'Customer' and tag['Value'] == 'ramseyst':
-#                     keep_instance = True
-#                 else:
-#                     if tag['Key'] == 'Name':
-#                        instance_name = tag['Value']
-#             if keep_instance:
-#                 result['list_data'].append({'instance-id': instance['InstanceId'],
-#                                             'instance-state': instance['State']['Name'],
-#                                             'instance-name': instance_name})
-#     result['status'] = 'success'
-#     result['status_message'] = ''
-# else:
-#     if command == "start":
-#         try:
-#             ec2.start_instances(InstanceIds=[req_instance_id])
-#             result['status'] = 'OK'
-#             result['status-message'] = ''
-#         except:
-#             result['status'] = 'error'
-#             result['status-message'] = str(sys.exc_info()[0])
-#     else:
-#         if command == "stop":
-#             try:
-#                 ec2.stop_instances(InstanceIds=[req_instance_id])
-#                 result['status'] = 'OK'
-#                 result['status-message'] = ''
-#             except:
-#                 result['status'] = 'error'
-#                 result['status-message'] = str(sys.exc_info()[0])
-#         else:
-#             result['status'] = 'error'
-#             result['status-message'] = 'invalid command: ' + command
-# print(json.dumps(result))
 
