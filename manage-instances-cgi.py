74a6e4d4c451173c6fc72ec8702737d7ffb984b3
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2018-03-10 09:53:36 -0800

    updated

--- manage-instances-cgi.py
+++ manage-instances-cgi.py
@@ -27,8 +27,8 @@ print("<title>Ramsey Lab EC2 Instance Manager</title>")
 print("</head>")
 print("<body>")
 print("<h1>Ramsey Lab EC2 Instance Manager</h1>")
-
 form = cgi.FieldStorage()
+print("<form method=\"GET\" action=\"/cgi-bin/manage-instances-cgi.py\">")
 if "command" in form:
     command = form["command"].value
     if "target_instance_id" not in form:
@@ -38,32 +38,43 @@ if "command" in form:
             print("<h2>Error: need to specify the manager passcode</h2>")
         else:
             target_instance_id = form["target_instance_id"]
-            manager_passcode = form["manager_passcode"]
+            target_manager_passcode = form["manager_passcode"]
             instance_info = ec2.describe_instance(InstanceIds=[target_instance_id])["Reservations"][0]["Instances"][0]
             instance_state = instance_info["State"]["Name"]
             instance_tags = instance_info["Tags"]
+            manager_passcode = ""
             for tag in instance_tags:
                 if tag["Key"] == "ManagerPasscode":
-                    if tag["Value"] != manager_passcode:
-                        print("<h2>Sorry:  Passcode not correct</h2>")
+                    manager_passcode = tag["Value"]
+            if manager_passcode == "":
+                print("<h2>Warning: instance has no manager passcode set</h2>")
+            if manager_passcode != target_manager_passcode:
+                print("<h2>Sorry:  Passcode not correct</h2>")
+            else:
+                if command == "start":
+                    if instance_state != "stopped":
+                        print("<h2>Error: Instance is not currently stopped, so I can not start it.</h2>")
                     else:
-                        if command == "start":
-                            if instance_state != "stopped":
-                                print("<h2>Error: Instance is not stopped, so you cannot start it</h2>")
-                            else:
-                                ec2.start_instances(InstanceIds=[target_instance_id])
+                        try:
+                            ec2.start_instances(InstanceIds=[target_instance_id])
+                        except:
+                            print("<h2>Error starting instance: " + str(sys.exec_info()[0]))
+                else:
+                    if command == "stop":
+                        if instance_state != "running":
+                            print("<h2>Error: Instance is not currently running, so I can not stop it.</h2>")
                         else:
-                            if command == "stop":
-                                if instance_state != "running":
-                                    print("<h2>Error: Instance is not running, so you cannot stop it</h2>")
-                                else:
-                                    ec2.start_instances(InstanceIds=[target_instance_id])
+                            try:
+                                ec2.stop_instances(InstanceIds=[target_instance_id])
+                            except:
+                                print("<h2>Error stopping instance: " + str(sys.exec_info()[0]))
            
 print("<table border=\"1\">")
 response = ec2.describe_instances()
 result['list_data'] = []
 manager_passcode = ''
 manager_passcodes = dict()
+print("<tr><td><em>Instance Name</em></td><td><em>Instance ID</em></td><td><em>Instance State</em></td></tr>")
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
         instance_tags = instance['Tags']
@@ -80,14 +91,20 @@ for reservation in response['Reservations']:
         if keep_instance:
             instance_id = instance["InstanceId"]
             manager_passcodes[instance_id] = manager_passcode
-            print("<tr><td>" + instance_name + "</td><td>" + instance["State"]["Name"] + "</td><td>" + instance_id + "</td></tr>")
+            print("<tr><td><input type=\"radio\" name=\"target_instance_id\" value=\"" + instance_id + "\" />" + instance_name + "</td><td>" + instance["State"]["Name"] + "</td><td>" + instance_id + "</td></tr>")
             
 #            result['list_data'].append({'instance-id': instance['InstanceId'],
 #                                        'instance-state': instance['State']['Name'],
 #                                        'instance-name': instance_name})
 print("</table>")
 
-        
+print("<p><input type=\"radio\" name=\"command\" value=\"start\" />Start instance</p>")
+print("<p><input type=\"radio\" name=\"command\" value=\"stop\" />Stop instance</p>")
+print("<p>Enter Management Passcode for your instance here: <input type=\"text\" name=\"manager_passcode\" /></p>")
+
+print("<p><input type=\"submit\" /></p>")
+
+print("</form>")
 print("</body>")
 print("</html>")
             
